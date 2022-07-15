8a7bf4838c2a5719efadd2836b4818dd58716638
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2022-07-15 09:25:46 +0700

    feat (✨): VARIABLE DECLARATIONS

--- src/github.com/socool/helloword/main.go
+++ src/github.com/socool/helloword/main.go
@@ -1,4 +1,20 @@
 package main
 func main() {
    println("Hello World")
+   // VARIABLE DECLARATIONS
+   println("TYPE 1")
+   var a,b,c bool;
+   println(a,":",b,":",c);
+
+   println("TYPE 2")
+   var d, e, f string;
+   println(d,":",e,":",f);
+
+   println("TYPE 3")
+   var g, h, i bool = false, false, true;
+   println(g,":",h,":",i);
+
+   println("TYPE 4")
+   var j, k, l = false, 10, "เรียนภาษา Go";
+   println(j,":",k,":",l);
 }
